d0a5605795e1596935bd8f73e38a3be29d474e88
khani.security <khani.security@gmail.com>
2024-12-02 14:26:53 +0330

    culture change

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -3,51 +3,35 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp1.Data;
 using BlazorApp1.DbContext;
-using BlazorApp1.Service;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Localization;
 using System.Globalization;
 using Telerik.Blazor.Services;
 using BlazorApp1.Services;
+using Microsoft.JSInterop;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#layout-wrapper");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddTelerikBlazor();
 builder.Services.AddScoped<IDataService, DataService>();
 builder.Services.AddSingleton<ApplicationDbContext>();
-builder.Services.AddSingleton<CultureService>();
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddTelerikBlazor();
 
 // register a custom localizer for the Telerik components, after registering the Telerik services
 builder.Services.AddSingleton(typeof(ITelerikStringLocalizer), typeof(ResxLocalizer));
+var host = builder.Build();
 
 #region Localization
+const string defaultCulture = "en-US";
+var js = host.Services.GetRequiredService<IJSRuntime>();
+var result = await js.InvokeAsync<string>("blazorCulture.get");
+var culture = CultureInfo.GetCultureInfo(result ?? defaultCulture);
 
-builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
-builder.Services.Configure<RequestLocalizationOptions>(options =>
-{
-    // define the list of cultures your app will support
-    var supportedCultures = new List<CultureInfo>()
-            {
-                new CultureInfo("en-US"),
-                new CultureInfo("de-DE"),
-                new CultureInfo("es-ES"),
-                new CultureInfo("bg-BG"),
-                new CultureInfo("fa-IR")
+if (result == null)
+    await js.InvokeVoidAsync("blazorCulture.set", defaultCulture);
 
-            };
-
-    // set the default culture
-    options.DefaultRequestCulture = new RequestCulture("fa-IR");
-
-    options.SupportedCultures = supportedCultures;
-    options.SupportedUICultures = supportedCultures;
-});
-
-// the custom localizer service is registered later, after the Telerik services
-
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
 #endregion
-
-await builder.Build().RunAsync();
+await host.RunAsync();
